a2396496e5c3c9e7f068a2fb9e8f699cbbab45f3
MANU SINGHAL <singhalmanu9@gmail.com>
2014-12-09 22:30:45 -0500

    setup commit and added Problem 1

---
+++ src/ProjectEuler.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Manu Singhal
+ */
+public class ProjectEuler 
+{
+    public static int problem1()
+    {
+        int sum = 0;
+        for(int i = 0; i < 1000; i ++)
+        {
+            if(i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        }
+        return sum;
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println(problem1());
+    }
+}
